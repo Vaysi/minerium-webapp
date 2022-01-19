14e92fe61f9a20cf52ce2b65922ef88289484ccd
Abolfazl Vaysi <vaysi.erfan@gmail.com>
2022-01-19 18:39:54 +0330

    Fix Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,14 @@
 import type {NextPage} from 'next'
-import {Box, Grid} from "@mui/material";
-import Header from "../components/header/header";
-import PageTitle from "../components/inline-components/page-title";
-import {Home as HomeIcon} from "@mui/icons-material";
-import Footer from "../components/footer/footer";
-import {useContext} from "react";
-import {userContext} from "../utils/context";
-
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 const Home: NextPage = () => {
-    const {user} = useContext(userContext);
+    const router = useRouter();
+    useEffect(() => {
+        router.push('/auth/login');
+    },[]);
     return (
-        <Grid container>
-            <Header/>
-            <PageTitle title={"Dashboard"} icon={<HomeIcon style={{width: 35, height: "auto"}}/>}/>
-            <Box sx={{width: "100%", height: 300}}>
-                {
-                    user?.loggedIn ? user?.displayName : 'test'
-                }
-            </Box>
-            <Footer/>
-        </Grid>
+        <>
+        </>
     );
 };
 
