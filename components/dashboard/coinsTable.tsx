cdee64ee7b8549c7a77fc2f3e88d7f79451ae1df
Abolfazl Vaysi <vaysi.erfan@gmail.com>
2022-03-19 14:04:35 +0330

    fix Mahsaa Reported Issues

--- components/dashboard/coinsTable.tsx
+++ components/dashboard/coinsTable.tsx
@@ -218,7 +218,7 @@ const CoinsTable = (props: Props) => {
                                                             pathname: "/calculator",
                                                             query: {coin: k}
                                                         })}>
-                                                            <CalculatorIcon className={"calcIcon"} styles={{height:60,width:"auto",fill: mode == 'light' ? "#043386" : "#D4E2F4"}} />
+                                                            <CalculatorIcon styles={{height:60,width:"auto",fill: mode == 'light' ? "#043386" : "#D4E2F4"}} />
                                                         </IconButton>
                                                     </Tooltip>
                                                 </div>
